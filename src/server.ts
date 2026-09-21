83d620482a13ba9be165e84da4d198b3e26203ec
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-09-21 11:35:12 -0500

    feat: habilitar acceso a la API desde la red local

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import { env } from './config/env';
 
-app.listen(env.port, () => {
+app.listen(env.port, '0.0.0.0', () => {
   console.log(`API Finanzas Hogar ejecutándose en http://localhost:${env.port}`);
   console.log(`Entorno: ${env.nodeEnv}`);
 });
